52d934b5515f0eec6c027d2c42182f873257081c
Ivan Liang <ivanwenliang@gmail.com>
2019-01-28 22:26:59 -0800

    Connect to Heroku

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -15,7 +15,7 @@ class SignIn extends React.Component {
         this.setState({signInPassword: event.target.value});
     }
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signIn', {
+        fetch('https://ancient-sands-49572.herokuapp.com/signIn', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
